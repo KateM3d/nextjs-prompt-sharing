92f9764e5d7a095ac34f451f1d550556d4acc351
KateM3d <ekaterina8medvedeva@gmail.com>
2023-09-30 11:15:16 -0400

    add auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,7 @@
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
 import nextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
-console.log({
-  clientId: process.env.GOOGLE_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-});
 
 const handler = nextAuth({
   providers: [
@@ -13,11 +10,34 @@ const handler = nextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.use.id = sessionUser._id.toString();
+
+    return session;
+  },
   async signIn({ profile }) {
     try {
+      await connectToDB();
+      //check if the user already excists
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+      // if not, create new user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
     } catch (error) {
       console.log(error);
+      return false;
     }
   },
 });
